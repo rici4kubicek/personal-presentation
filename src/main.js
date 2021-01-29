a96c906fb27ad4663aa85bbb81cb4054e65a02e1
Richard Kubíček <rici.kubicek@gmail.com>
2021-01-29 21:33:55 +0100

    Use router

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import VueRouter from 'vue-router'
+import { provideRouter } from './router/index.js'
 
-Vue.use(VueRouter)
 Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
+let router = provideRouter()
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
