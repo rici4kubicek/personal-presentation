005b18c12f23b96f512238fa490a496a3edfd534
Richard Kubíček <rici.kubicek@gmail.com>
2021-01-30 10:40:43 +0100

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import provideRouter from './router/index.js'
 import i18n from './i18n'
+import VueAnalytics from 'vue-analytics';
 
 Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
 let router = provideRouter()
+
+Vue.use(VueAnalytics, {
+  id: 'G-23R4NZE94Q',
+  router
+});
 
 new Vue({
   router,
