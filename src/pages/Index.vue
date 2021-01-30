f41e97f6d2d768cf812d39eaec34eca69cac5ea5
Richard Kubíček <rici.kubicek@gmail.com>
2021-01-30 04:09:27 +0100

    update index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,11 +14,6 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque
           ea, nostrum odio. Dolores, sed accusantium quasi non.
         </md-card-content>
-
-        <md-card-actions>
-          <md-button>Action</md-button>
-          <md-button>Action</md-button>
-        </md-card-actions>
       </md-ripple>
     </md-card>
 
@@ -33,11 +28,6 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque
           ea, nostrum odio. Dolores, sed accusantium quasi non.
         </md-card-content>
-
-        <md-card-actions>
-          <md-button>Action</md-button>
-          <md-button>Action</md-button>
-        </md-card-actions>
       </md-ripple>
     </md-card>
   </div>
